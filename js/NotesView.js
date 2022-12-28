22230104067fd1513cf15fa70212d9c5d786a06c
TheMaxON <sldpsnow8800@gmail.com>
2022-12-28 03:05:48 +0200

    Fixes, broken animation

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,10 +1,8 @@
 export default class NotesView {
-    constructor(root, {
-        onNoteSelect,
-        onNoteAdd,
-        onNoteEdit,
-        onNoteDelete
-    } = {}) {
+    constructor(
+        root,
+        { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}
+    ) {
         this.root = root;
         this.onNoteSelect = onNoteSelect;
         this.onNoteAdd = onNoteAdd;
@@ -32,16 +30,15 @@ export default class NotesView {
             </div>
         `;
 
-        const btnAddNote = this.root.querySelector(".menu__add-btn");
-        const inpTitle = this.root.querySelector(".note__title");
-        const inpBody = this.root.querySelector(".note__body");
+        const btnAddNote = this.root.querySelector('.menu__add-btn');
+        const inpTitle = this.root.querySelector('.note__title');
+        const inpBody = this.root.querySelector('.note__body');
 
-        btnAddNote.addEventListener("click", () => {
+        btnAddNote.addEventListener('click', () => {
             this.onNoteAdd();
         });
-
-        [inpTitle, inpBody].forEach(inputField => {
-            inputField.addEventListener("blur", () => {
+        [inpTitle, inpBody].forEach((inputField) => {
+            inputField.addEventListener('blur', () => {
                 const updatedTitle = inpTitle.value.trim();
                 const updatedBody = inpBody.value.trim();
 
@@ -60,71 +57,83 @@ export default class NotesView {
             <div class="menu__list-preview-name">${title}</div>
             <div class="menu__list-preview-text">
                     ${body.substring(0, MAX_BODY_LENGTH)}
-                    ${body.length > MAX_BODY_LENGTH ? "..." : ""}
+                    ${body.length > MAX_BODY_LENGTH ? '...' : ''}
                 </div>
                 <div class="menu__list-preview-date">
-                    ${updated.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                    ${updated.toLocaleString(undefined, {
+                        dateStyle: 'full',
+                        timeStyle: 'short',
+                    })}
                 </div>
             </div>
         `;
     }
 
     updateNoteList(notes) {
-        const notesListContainer = this.root.querySelector(".menu__list");
+        const notesListContainer = this.root.querySelector('.menu__list');
 
         // Empty list
-        notesListContainer.innerHTML = "";
+        notesListContainer.innerHTML = '';
 
         for (const note of notes) {
-            const html = this._createListItemHTML(note.id, note.title, note.body, new Date(note.updated));
+            const html = this._createListItemHTML(
+                note.id,
+                note.title,
+                note.body,
+                new Date(note.updated)
+            );
 
-            notesListContainer.insertAdjacentHTML("beforeend", html);
+            notesListContainer.insertAdjacentHTML('beforeend', html);
         }
 
         // Add select/delete events for each list item
         const menu = document.querySelector('.menu');
-        const note = document.querySelector('.note__body')
-        const noteOther = document.querySelector('.menu__list')
-        notesListContainer.querySelectorAll(".menu__list-item").forEach(noteListItem => {
-            noteListItem.addEventListener("click", () => {
-                
-                noteOther.classList.add('notes-open_anim')
-                
-                setTimeout(() => {
-                    menu.style.display = 'none';
-                    noteOther.classList.remove('notes-open_anim')
-                    
-                    note.classList.add('text-open_anim')
-                    menu.classList.remove('side-bar_anim')
-                }, 500);
-                this.onNoteSelect(noteListItem.dataset.noteId);
-            });
-
-            noteListItem.addEventListener("dblclick", () => {
-                const doDelete = confirm("Are you sure you want to permanently delete this note?");
-
-                if (doDelete) {
-                    this.onNoteDelete(noteListItem.dataset.noteId);
+        const note = document.querySelector('.note__body');
+        notesListContainer
+            .querySelectorAll('.menu__list-item')
+            .forEach((noteListItem) => {
+                noteListItem.addEventListener('click', () => {
                     setTimeout(() => {
-                        menu.style.display = 'initial';
-                    }, 1);
-                }
+                        menu.classList.add('menu__close-anim');
+                        note.classList.add('note__open-anim');
+                    }, 500);
+                    console.log('opening animation');
+                    this.onNoteSelect(noteListItem.dataset.noteId);
+                });
+
+                noteListItem.addEventListener('dblclick', () => {
+                    const doDelete = confirm(
+                        'Are you sure you want to permanently delete this note?'
+                    );
+
+                    if (doDelete) {
+                        this.onNoteDelete(noteListItem.dataset.noteId);
+                        setTimeout(() => {
+                            menu.style.display = 'initial';
+                        }, 1);
+                    }
+                });
             });
-        });
     }
 
     updateActiveNote(note) {
-        this.root.querySelector(".note__title").value = note.title;
-        this.root.querySelector(".note__body").value = note.body;
+        this.root.querySelector('.note__title').value = note.title;
+        this.root.querySelector('.note__body').value = note.body;
 
-        this.root.querySelectorAll(".menu__list-item").forEach(noteListItem => {
-            noteListItem.classList.remove("menu__list-item-selected");
-        });
+        this.root
+            .querySelectorAll('.menu__list-item')
+            .forEach((noteListItem) => {
+                noteListItem.classList.remove('menu__list-item-selected');
+            });
 
-        this.root.querySelector(`.menu__list-item[data-note-id="${note.id}"]`).classList.add("menu__list-item-selected");
+        this.root
+            .querySelector(`.menu__list-item[data-note-id="${note.id}"]`)
+            .classList.add('menu__list-item-selected');
     }
 
     updateNotePreviewVisibility(visible) {
-        this.root.querySelector(".note").style.visibility = visible ? "visible" : "hidden";
+        this.root.querySelector('.note').style.visibility = visible
+            ? 'visible'
+            : 'hidden';
     }
 }
--- js/engine.js
+++ js/engine.js
@@ -1,144 +1,144 @@
 //LOADER
-window.addEventListener("load", function () {
-  var loader = document.querySelector('.skeleton')
-  setTimeout(() => {
-    loader.style.display = "none"
-  }, 500);
-})
-
-// window.addEventListener("load", function () {
-//   var loader = document.querySelector('.preloader')
-//   setTimeout(() => {
-//     loader.style.display = "none"
-//   }, 500);
-// })
+window.addEventListener('load', function () {
+    var loader = document.querySelector('.skeleton');
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 500);
+});
 
 //SIDE-BAR
-function navSlide() {
-  const nav = document.querySelector('.menu');
-  const note = document.querySelector('.note__body')
-  note.classList.add('text-open_anim_reverse')
-  note.classList.remove('text-open_anim')
-  setTimeout(() => {
-    nav.classList.add('side-bar_anim')
-    nav.style.display = 'initial'
-    note.classList.remove('text-open_anim_reverse')
-  }, 500);
-}
-
-//MENU-ANIMATIONS
-
-
-// function navSlide() {
-//   const nav = document.querySelector('.menu');
-//   const note = document.querySelector('.note__body')
-//   note.classList.add('text-open_anim_reverse')
-//   note.classList.remove('text-open_anim')
-//   setTimeout(() => {
-//     nav.classList.add('side-bar_anim')
-//     nav.style.display = 'initial'
-//     note.classList.remove('text-open_anim_reverse')
-//   }, 500);
-// }
+const menuListBtn = document.querySelector('#header__menu-btn');
+menuListBtn.addEventListener('click', function () {
+    const menu = document.querySelector('.menu');
+    const note = document.querySelector('.note');
+    menu.classList.remove('menu__close-anim');
+    note.classList.remove('note__open-anim');
+    note.classList.add('note__close-anim');
+    setTimeout(() => {
+        menu.classList.add('menu__open-anim');
+        console.log('closing animation done');
+    }, 1000);
+});
 
 //SCREENSHOT
 function screenshotMode() {
-  const header = document.querySelector('.header');
-  const note = document.querySelector('.note__body');
-  const btn = document.querySelector('.screenshot-btn');
+    const header = document.querySelector('.header');
+    const note = document.querySelector('.note__body');
+    const btn = document.querySelector('.screenshot-btn');
 
-  header.style.display = 'none';
-  note.style.height = '800vh';
-  btn.style.display = 'initial';
+    header.style.display = 'none';
+    note.style.height = '800vh';
+    btn.style.display = 'initial';
 }
 
 //SETTINGS DROP-DOWN
-const settingBtn = document.querySelector('#option-btn')
-const dropdown = document.querySelector('.settings')
-const dropdownMenu = document.querySelector('.settings__menu')
-const closeBtn = document.querySelector('.settings__close')
-const settingsList = document.querySelector('.settings__menu-list')
+const settingBtn = document.querySelector('#option-btn');
+const dropdown = document.querySelector('.settings');
+const dropdownMenu = document.querySelector('.settings__menu');
+const closeBtn = document.querySelector('.settings__close');
+const settingsList = document.querySelector('.settings__menu-list');
 
-var canClose = false
+var canClose = false;
 
 settingBtn.addEventListener('click', function () {
-  dropdown.style.display = 'initial'
-})
+    dropdown.style.display = 'initial';
+});
 
-dropdownMenu.addEventListener('mouseover', function() {
-  canClose = false
-})
+dropdownMenu.addEventListener('mouseover', function () {
+    canClose = false;
+});
 
-dropdownMenu.addEventListener('mouseout', function() {
-  canClose = true
-})
+dropdownMenu.addEventListener('mouseout', function () {
+    canClose = true;
+});
 
-dropdown.addEventListener('click', function() {
-  if (canClose == true) {
-    dropdown.style.display = 'none'
-  }
-})
+dropdown.addEventListener('click', function () {
+    if (canClose == true) {
+        dropdown.style.display = 'none';
+    }
+});
 
-closeBtn.addEventListener('click', function() {
-  dropdown.style.display = 'none'
-})
+closeBtn.addEventListener('click', function () {
+    dropdown.style.display = 'none';
+});
 
 //FONTS
 
 function sansSerifFont() {
-  const note = document.querySelector('.note__body')
+    const note = document.querySelector('.note__body');
 
-  note.style.fontFamily = 'Mulish'
+    note.style.fontFamily = 'Mulish';
 }
 
 function serifFont() {
-  const note = document.querySelector('.note__body')
+    const note = document.querySelector('.note__body');
 
-  note.style.fontFamily = 'Gentium Book Plus'
+    note.style.fontFamily = 'Gentium Book Plus';
 }
 
 //THEMES
 
 function defaultTheme() {
-  document.documentElement.style.setProperty('--sidebarBackground', 'linear-gradient(0deg, #ab8b89 0%, #1a517e 100%)');
-  document.documentElement.style.setProperty('--theme-color', '#FCEC7B');
-  document.documentElement.style.setProperty('--accent-text-color', 'black');
-  localStorage.setItem('notesTheme', 'default')
+    document.documentElement.style.setProperty(
+        '--sidebarBackground',
+        'linear-gradient(0deg, #ab8b89 0%, #1a517e 100%)'
+    );
+    document.documentElement.style.setProperty('--theme-color', '#FCEC7B');
+    document.documentElement.style.setProperty('--accent-text-color', 'black');
+    localStorage.setItem('notesTheme', 'default');
 }
 
 function seaFoamGreenTheme() {
-  document.documentElement.style.setProperty('--sidebarBackground', 'linear-gradient(0deg, rgba(31,64,55,1) 0%, rgba(153,242,200,1) 100%)');
-  document.documentElement.style.setProperty('--theme-color', '#57855e');
-  document.documentElement.style.setProperty('--accent-text-color', '#e8daa3');
-  localStorage.setItem('notesTheme', 'seaFoamGreenTheme')
+    document.documentElement.style.setProperty(
+        '--sidebarBackground',
+        'linear-gradient(0deg, rgba(31,64,55,1) 0%, rgba(153,242,200,1) 100%)'
+    );
+    document.documentElement.style.setProperty('--theme-color', '#57855e');
+    document.documentElement.style.setProperty(
+        '--accent-text-color',
+        '#e8daa3'
+    );
+    localStorage.setItem('notesTheme', 'seaFoamGreenTheme');
 }
 
 function cyberpunkTheme() {
-  document.documentElement.style.setProperty('--sidebarBackground', 'linear-gradient(180deg, rgba(0,13,28,1) 0%, rgba(3,45,49,1) 50%, rgba(32,40,90,1) 100%)');
-  document.documentElement.style.setProperty('--theme-color', '#e43736');
-  document.documentElement.style.setProperty('--accent-text-color', 'white');
-  localStorage.setItem('notesTheme', 'cyberpunkTheme')
+    document.documentElement.style.setProperty(
+        '--sidebarBackground',
+        'linear-gradient(180deg, rgba(0,13,28,1) 0%, rgba(3,45,49,1) 50%, rgba(32,40,90,1) 100%)'
+    );
+    document.documentElement.style.setProperty('--theme-color', '#e43736');
+    document.documentElement.style.setProperty('--accent-text-color', 'white');
+    localStorage.setItem('notesTheme', 'cyberpunkTheme');
 }
 
 function expressionTheme() {
-  document.documentElement.style.setProperty('--sidebarBackground', 'linear-gradient(321deg, rgba(142,45,226,1) 0%, rgba(74,0,224,1) 100%)');
-  document.documentElement.style.setProperty('--theme-color', '#00e7b2');
-  document.documentElement.style.setProperty('--accent-text-color', '#000712');
-  localStorage.setItem('notesTheme', 'expressionTheme')
+    document.documentElement.style.setProperty(
+        '--sidebarBackground',
+        'linear-gradient(321deg, rgba(142,45,226,1) 0%, rgba(74,0,224,1) 100%)'
+    );
+    document.documentElement.style.setProperty('--theme-color', '#00e7b2');
+    document.documentElement.style.setProperty(
+        '--accent-text-color',
+        '#000712'
+    );
+    localStorage.setItem('notesTheme', 'expressionTheme');
 }
 
-window.onload = themeCheck()
+window.onload = themeCheck();
 
 function themeCheck() {
-  var currentTheme = localStorage.getItem('notesTheme')
+    var currentTheme = localStorage.getItem('notesTheme');
 
-  if (currentTheme === 'default') {
-    defaultTheme()
-  }  if (currentTheme === 'seaFoamGreenTheme') {
-    seaFoamGreenTheme()
-  }  if (currentTheme === 'cyberpunkTheme') {
-    cyberpunkTheme()
-  }  if (currentTheme === 'expressionTheme') {
-    expressionTheme()
-  }
+    if (currentTheme === 'default') {
+        defaultTheme();
+    }
+    if (currentTheme === 'seaFoamGreenTheme') {
+        seaFoamGreenTheme();
+    }
+    if (currentTheme === 'cyberpunkTheme') {
+        cyberpunkTheme();
+    }
+    if (currentTheme === 'expressionTheme') {
+        expressionTheme();
+    }
 }
